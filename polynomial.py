436bff7e75c3e837668a27d6693725eabfe47dbe
will18870 <will18870@live.com>
2024-01-28 13:48:03 -0500

    cs 506 exercise part c

--- polynomial.py
+++ polynomial.py
@@ -1,9 +1,17 @@
+# William Huang
+# evaluate() acts kind of like a pseudo-recursive method, where each evaluate() call
+# calls the evaluate() method of the class that makes up the variables of the current class
+# until it inevitably reaches X or Int, in which it returns the raw value of those classes.
+
 class X:
     def __init__(self):
         pass
 
     def __repr__(self):
         return "X"
+
+    def evaluate(self, x):
+        return x
 
 class Int:
     def __init__(self, i):
@@ -12,6 +20,9 @@ class Int:
     def __repr__(self):
         return str(self.i)
 
+    def evaluate(self, x):
+        return self.i
+
 class Add:
     def __init__(self, p1, p2):
         self.p1 = p1
@@ -19,6 +30,9 @@ class Add:
 
     def __repr__(self):
         return repr(self.p1) + " + " + repr(self.p2)
+
+    def evaluate(self, x):
+        return self.p1.evaluate(x) + self.p2.evaluate(x)
 
 class Sub:
     def __init__(self, p1, p2):
@@ -29,6 +43,10 @@ class Sub:
         if isinstance(self.p2, (Add, Sub, Mul, Div)):
             return repr(self.p1) + " - ( " + repr(self.p2) + " )"
         return repr(self.p1) + " - " + repr(self.p2)
+
+    def evaluate(self, x):
+        return self.p1.evaluate(x) - self.p2.evaluate(x)
+
 
 class Mul:
     def __init__(self, p1, p2):
@@ -44,6 +62,9 @@ class Mul:
             return repr(self.p1) + " * ( " + repr(self.p2) + " )"
         return repr(self.p1) + " * " + repr(self.p2)
 
+    def evaluate(self, x):
+        return self.p1.evaluate(x) * self.p2.evaluate(x)
+
 class Div:
     def __init__(self, p1, p2):
         self.p1 = p1
@@ -58,6 +79,9 @@ class Div:
             return repr(self.p1) + " / ( " + repr(self.p2) + " )"
         return repr(self.p1) + " / " + repr(self.p2)
 
+    def evaluate(self, x):
+        return self.p1.evaluate(x) / self.p2.evaluate(x)
 
-poly = Add( Add( Int(4), Int(3)), Sub( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
+poly = Sub( Add( Int(4), Int(3)), Add( X(), Mul( Int(3), Add( Mul(X(), X()), Int(1)))))
 print(poly)
+print(poly.evaluate(2))
